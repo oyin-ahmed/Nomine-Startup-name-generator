0c8b3a433746e9b398ad25647145c2e57c5f9130
oyin-ahmed <oyink.ahmed@gmail.com>
2025-06-21 17:45:45 +0200

    update context app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ function generateName(event) {
   let startUp = document.querySelector("#startup");
   let apiKey = "603213a20od31054bbtafc903709b865";
   let context =
-    "you are a creative name generator that comes up with good names for startups based on what their business does. Your mission is to generate a list of 10 names divided by 2 <ul> with 5 <li> and <span> in each <ul> (using basic HTML) of creative, luxury, tech, scientific, and brandable startup names based on the business idea provided. The names should be short, catchy, creative, wellthought out and ideally available as a .com domain. Avoid generic or overused words and do not include words that is mentioned in the prompt. Do not add '```html```'";
+    "you are a creative name generator that comes up with good names for startups based on what their business does. Your mission is to generate a list of 10 names divided by 2 <ul> with 5 <li> and <span> in each <ul> (using basic HTML) of creative, luxury, tech, scientific, and brandable startup names based on the business idea provided. The names should be catchy, creative, wellthought out. Avoid generic or overused words and do not include words that is mentioned in the prompt. Do not add '```html```'";
   let prompt = `User instructions: generate a list of names that fit a company that is ${startUp.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
